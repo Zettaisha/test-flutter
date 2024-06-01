eee5e2a036f76789d64336960b16271b27ee63c2
Zettaisha <levvotintsev18@gmail.com>
2024-06-01 18:09:19 +0300

    open article page on tap

---
+++ lib/presentation/widgets/article_page.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+import 'package:test_flutter/data/models/article.dart';
+
+class ArticlePage extends StatelessWidget {
+  const ArticlePage({Key? key, required this.article}) : super(key: key);
+  final Article article;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(
+              height: 400,
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(12),
+                child: Stack(
+                  children: [
+                    Positioned.fill(
+                      child: ColorFiltered(
+                        colorFilter: ColorFilter.mode(
+                          Colors.black.withOpacity(0.5),
+                          BlendMode.srcATop,
+                        ),
+                        child: Container(
+                          decoration: BoxDecoration(
+                            image: DecorationImage(
+                              image: NetworkImage(article.imageUrl),
+                              fit: BoxFit.fill,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Positioned(
+                      top: 60,
+                      left: 20,
+                      child: IconButton(
+                        icon: const Icon(Icons.arrow_back_ios,
+                            color: Colors.white),
+                        onPressed: () {
+                          Navigator.of(context).pop();
+                        },
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(40.0),
+                      child: Align(
+                        alignment: Alignment.bottomLeft,
+                        child: Text(
+                          article.title,
+                          style: const TextStyle(
+                            color: Colors.white,
+                            fontSize: 28,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Text(
+                article.description ?? 'There is no text',
+                style: const TextStyle(
+                  fontSize: 16,
+                ),
+                textAlign: TextAlign.justify,
+                softWrap: true,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/presentation/widgets/featured_news_card.dart
+++ lib/presentation/widgets/featured_news_card.dart
@@ -1,51 +1,58 @@
 import 'package:flutter/material.dart';
 import 'package:test_flutter/data/models/article.dart';
+import 'package:test_flutter/presentation/widgets/article_page.dart';
 
 class FeaturedNewsCard extends StatelessWidget {
   const FeaturedNewsCard({Key? key, required this.article}) : super(key: key);
   final Article article;
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: 270,
-      width: 380,
-      child: ClipRRect(
-        borderRadius: BorderRadius.circular(12),
-        child: Stack(
-          children: [
-            Positioned.fill(
-              child: ColorFiltered(
-                colorFilter: ColorFilter.mode(
-                  Colors.black.withOpacity(0.5),
-                  BlendMode.srcATop,
-                ),
-                child: Container(
-                  decoration: BoxDecoration(
-                    image: DecorationImage(
-                      image: NetworkImage(article.imageUrl),
-                      fit: BoxFit.cover,
+    return GestureDetector(
+      child: SizedBox(
+        height: 270,
+        width: 380,
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(12),
+          child: Stack(
+            children: [
+              Positioned.fill(
+                child: ColorFiltered(
+                  colorFilter: ColorFilter.mode(
+                    Colors.black.withOpacity(0.5),
+                    BlendMode.srcATop,
+                  ),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: NetworkImage(article.imageUrl),
+                        fit: BoxFit.cover,
+                      ),
                     ),
                   ),
                 ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(20.0),
-              child: Align(
-                alignment: Alignment.bottomLeft,
-                child: Text(
-                  article.title,
-                  style: const TextStyle(
-                    color: Colors.white,
-                    fontSize: 28,
-                    fontWeight: FontWeight.w500,
+              Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Align(
+                  alignment: Alignment.bottomLeft,
+                  child: Text(
+                    article.title,
+                    style: const TextStyle(
+                      color: Colors.white,
+                      fontSize: 28,
+                      fontWeight: FontWeight.w500,
+                    ),
                   ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
+      onTap: () {
+        Navigator.of(context).push(
+            MaterialPageRoute(builder: (ctx) => ArticlePage(article: article)));
+      },
     );
   }
 }
